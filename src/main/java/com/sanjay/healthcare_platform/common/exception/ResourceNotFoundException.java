8462395bb694b8d22b98111ce2d176549b171d20
Sanjay Devarjankar <sanjay.d@medtrixhealthcare.com>
2026-09-15 14:18:37 +0530

    clean up

--- src/main/java/com/sanjay/healthcare_platform/common/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.sanjay.healthcare_platform.common.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
